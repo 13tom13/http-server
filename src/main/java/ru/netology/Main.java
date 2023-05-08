ce6307f3d5d95a889adfac2f1c4dd7fd2e7a86e7
13tom13 <13tom13@mail.ru>
2023-05-08 16:13:25 +0200

    add start method for Server

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,11 +1,11 @@
 package ru.netology;
 
-import static ru.netology.Server.startServer;
-
 public class Main {
-  public static void main(String[] args) {
-    startServer(9999);
-  }
+
+    private static final int PORT = 9999;
+    public static void main(String[] args) {
+        new Server(PORT);
+    }
 }
 
 
