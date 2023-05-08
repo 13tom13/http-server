1cd14eb496963772504b6e04c4335757e6fc4273
13tom13 <13tom13@mail.ru>
2023-05-08 19:11:37 +0200

    add connection method for Server

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,10 +1,19 @@
 package ru.netology;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     private static final int PORT = 9999;
+
+    private static final int THREAD_COUNT = 64;
+
+    private static final ExecutorService THREADPOOL = Executors.newFixedThreadPool(THREAD_COUNT);
+
     public static void main(String[] args) {
-        new Server(PORT);
+        Server server = new Server(PORT, THREADPOOL);
+        server.start();
     }
 }
 
